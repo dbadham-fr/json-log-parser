dc4cf2ee6d42a272114c7e8dda217e419dcb67ca
Dan Badham <dan.badham@forgerock.com>
2024-06-14 15:48:34 +0100

    Add unit test for log formatting

--- cmd/json-log-parser_test.go
+++ cmd/json-log-parser_test.go
@@ -6,20 +6,28 @@ import (
 	"testing"
 )
 
-var sampleLog = map[string]interface{}{
-	"level":     "debug",
+var logWithException = map[string]interface{}{
+	"level":     "DEBUG",
 	"timestamp": "2024-06-10T08:47:35.469Z",
 	"thread":    "Thread-1",
 	"logger":    "Logger-1",
 	"message":   "Something bad happened when processing this request - see exception for details",
-	"exception": "java.lang.NullPointerException: null\\n\\tat java.base/java.util.Objects.requireNonNull(Unknown Source)\\n\\tat org.forgerock.http.vertx.server.StreamedBodyHandler.<init>(StreamedBodyHandler.java:35)\\n\\tat org.forgerock.http.vertx.server.ChfApplicationWebHandler.handleRequestBody(ChfApplicationWebHandler.java:275)\\n\\tat org.forgerock.http.vertx.server.ChfApplicationWebHandler.handle0(ChfApplicationWebHandler.java:202)\\n\\tat org.forgerock.http.vertx.server.ChfApplicationWebHandler.handle(ChfApplicationWebHandler.java:167)\\n\\tat org.forgerock.http.vertx.server.ChfApplicationWebHandler.handle(ChfApplicationWebHandler.java:93)\\n\\tat org.forgerock.openig.launcher.IdentityGatewayVerticle.adminEndpointRouter(IdentityGatewayVerticle.java:99)\\n\\tat io.vertx.lang.rx.DelegatingHandler.handle(DelegatingHandler.java:20)\\n\\tat io.vertx.core.http.impl.Http1xServerRequestHandler.handle(Http1xServerRequestHandler.java:67)\\n\\tat io.vertx.core.http.impl.Http1xServerRequestHandler.handle(Http1xServerRequestHandler.java:30)\\n\\tat io.vertx.core.impl.ContextImpl.emit(ContextImpl.java:335)\\n\\tat io.vertx.core.impl.DuplicatedContext.emit(DuplicatedContext.java:176)\\n\\tat io.vertx.core.http.impl.Http1xServerConnection.handleMessage(Http1xServerConnection.java:174)\\n\\tat io.vertx.core.net.impl.ConnectionBase.read(ConnectionBase.java:159)\\n\\tat io.vertx.core.net.impl.VertxHandler.channelRead(VertxHandler.java:153)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:442)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.fireChannelRead(AbstractChannelHandlerContext.java:412)\\n\\tat io.netty.channel.ChannelInboundHandlerAdapter.channelRead(ChannelInboundHandlerAdapter.java:93)\\n\\tat io.netty.handler.codec.http.websocketx.extensions.WebSocketServerExtensionHandler.onHttpRequestChannelRead(WebSocketServerExtensionHandler.java:160)\\n\\tat io.netty.handler.codec.http.websocketx.extensions.WebSocketServerExtensionHandler.channelRead(WebSocketServerExtensionHandler.java:83)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:442)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.fireChannelRead(AbstractChannelHandlerContext.java:412)\\n\\tat io.vertx.core.http.impl.Http1xUpgradeToH2CHandler.channelRead(Http1xUpgradeToH2CHandler.java:124)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:444)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.fireChannelRead(AbstractChannelHandlerContext.java:412)\\n\\tat io.netty.handler.codec.ByteToMessageDecoder.fireChannelRead(ByteToMessageDecoder.java:346)\\n\\tat io.netty.handler.codec.ByteToMessageDecoder.channelRead(ByteToMessageDecoder.java:318)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:444)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.fireChannelRead(AbstractChannelHandlerContext.java:412)\\n\\tat io.vertx.core.http.impl.Http1xOrH2CHandler.end(Http1xOrH2CHandler.java:61)\\n\\tat io.vertx.core.http.impl.Http1xOrH2CHandler.channelRead(Http1xOrH2CHandler.java:38)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:444)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.fireChannelRead(AbstractChannelHandlerContext.java:412)\\n\\tat io.netty.channel.DefaultChannelPipeline$HeadContext.channelRead(DefaultChannelPipeline.java:1410)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:440)\\n\\tat io.netty.channel.AbstractChannelHandlerContext.invokeChannelRead(AbstractChannelHandlerContext.java:420)\\n\\tat io.netty.channel.DefaultChannelPipeline.fireChannelRead(DefaultChannelPipeline.java:919)\\n\\tat io.netty.channel.nio.AbstractNioByteChannel$NioByteUnsafe.read(AbstractNioByteChannel.java:166)\\n\\tat io.netty.channel.nio.NioEventLoop.processSelectedKey(NioEventLoop.java:788)\\n\\tat io.netty.channel.nio.NioEventLoop.processSelectedKeysOptimized(NioEventLoop.java:724)\\n\\tat io.netty.channel.nio.NioEventLoop.processSelectedKeys(NioEventLoop.java:650)\\n\\tat io.netty.channel.nio.NioEventLoop.run(NioEventLoop.java:562)\\n\\tat io.netty.util.concurrent.SingleThreadEventExecutor$4.run(SingleThreadEventExecutor.java:997)\\n\\tat io.netty.util.internal.ThreadExecutorMap$2.run(ThreadExecutorMap.java:74)\\n\\tat io.netty.util.concurrent.FastThreadLocalRunnable.run(FastThreadLocalRunnable.java:30)\\n\\tat java.base/java.lang.Thread.run(Unknown Source)\\n",
+	"exception": "java.lang.NullPointerException: null\\n\\tat java.base/java.util.Objects.requireNonNull(Unknown Source)\\n\\tat redacted",
+}
+
+var logWithoutException = map[string]interface{}{
+	"level":     "INFO",
+	"timestamp": "2024-06-10T08:47:35.444Z",
+	"thread":    "Thread-4",
+	"logger":    "Logger-3",
+	"message":   "Something of note happened",
 }
 
 var builder = &strings.Builder{}
 
 func BenchmarkLogPrintingWithBuilder(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		formatLog(builder, sampleLog)
+		formatLog(builder, logWithException)
 		builder.Reset()
 	}
 }
@@ -45,4 +53,22 @@ func TestParseSeverity(t *testing.T) {
 		assert.Equal(t, scenarios[desc], parseSeverity(desc))
 	}
 
+}
+
+func TestFormatLog(t *testing.T) {
+	var b = &strings.Builder{}
+	formatLog(b, logWithException)
+
+	assert.Equal(t, "2024-06-10T08:47:35.469Z [Thread-1] DEBUG Logger-1 "+
+		"Something bad happened when processing this request - see exception for details "+
+		"java.lang.NullPointerException: null\\n\\tat java.base/java.util.Objects.requireNonNull(Unknown Source)\\n"+
+		"\\tat redacted", b.String())
+}
+
+func TestFormatLogNoException(t *testing.T) {
+	var b = &strings.Builder{}
+	formatLog(b, logWithoutException)
+
+	assert.Equal(t, "2024-06-10T08:47:35.444Z [Thread-4] INFO Logger-3 "+
+		"Something of note happened", b.String())
 }
